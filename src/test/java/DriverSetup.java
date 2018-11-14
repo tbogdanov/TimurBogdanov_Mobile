7bafc7e34de409fede8b7ee898bc787af1db9bd1
tbogdanov <gion22@gmail.com>
2018-11-14 22:54:58 +0300

    Initial commit. Skeleton for HW2 described in class

---
+++ src/test/java/DriverSetup.java
@@ -0,0 +1,31 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DriverSetup {
+    protected AndroidDriver driver;
+
+    protected void prepareNative() throws MalformedURLException { // exception required by java.net.URL
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //mandatory capabilities
+        capabilities.setCapability("deviceName","emulator-5554");
+        capabilities.setCapability("platformName","Android");
+
+        // path to app
+        // Copy the application (.apk), which will become AUT, to the specified location,
+        // e.g. "resources" folder of the project
+        File appDir = new File
+                ("src/main/resources");
+        File app = new File(appDir, "ContactManager.apk");
+
+        //other caps
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        // Init driver for local Appium server with capabilities have been set
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+    }
+}
